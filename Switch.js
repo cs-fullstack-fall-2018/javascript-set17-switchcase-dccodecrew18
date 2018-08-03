d5144d1503878fa5ec81bd150fa0107306cf762a
Darius Carter <dcarterbusinessemail@gmail.com>
2018-08-03 13:20:09 -0500

    First push

---
+++ Switch.js
@@ -0,0 +1,25 @@
+
+var inputNumber = parseInt(prompt("Enter a number "));
+function main(){
+
+    switch(inputNumber) {
+        case 1:
+            console.log(1);
+            break;
+        case 2:
+            console.log(2);
+            break;
+        case 3:
+            console.log(3);
+            break;
+        case 4:
+            console.log(4);
+            break;
+        case 5:
+            console.log(5);
+            break;
+        default:
+            console.log(" Enter a number 1 - 5")
+     }
+    }
+main();
